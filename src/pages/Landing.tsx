66d4759186e95a33531832ab2ae027513b50cf51
anhnguyen6535 <anhnguyen6535@gmail.com>
2024-12-04 23:11:18 -0700

    Display all photos

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,25 @@
+import { IonContent, IonHeader, IonPage, IonTitle } from '@ionic/react';
 import type { FC } from 'react';
+import { photoDetail } from './photoData';
 
 interface LandingProps {}
 
 const Landing: FC<LandingProps> = () => {
     return (
-        <>
-            Landing
-        </>
+        <IonPage>
+            <IonContent fullscreen className="ion-padding">
+                <h1>Frozen Concrete</h1>
+                
+                {photoDetail.map((photo, index) =>(
+                    <div key={index}>
+                        <h2>{photo.header}</h2>
+                        <img alt={photo.header} src={photo.src}/>
+                        <p>{photo.detail}</p>
+                    </div>
+                )
+                )}
+            </IonContent>
+        </IonPage>
     );
 }
 
